4c08abfd49ad1180259c7caa7595c8cdc3acf254
Tim Brooks <brooks@cern.ch>
2018-02-28 21:56:25 +0000

    Add license and readme
    README.md generated from
    [cargo-readme](https://github.com/livioribeiro/cargo-readme)

---
+++ README.tpl
@@ -0,0 +1,6 @@
+# {{crate}}
+
+{{readme}}
+
+
+License: {{license}}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+//! A reddit bot shamelessly reimplementing [Decronym](http://decronym.xyz/) but in
+//! [Rust](https://www.rust-lang.org/) and for the
+//! [/r/rust subreddit](https://www.reddit.com/r/rust/).
+//!
+//! # Installation
+//!
+//!     cargo install diesel
+//!     git clone https://github.com/brookst/rcronym.git
+//!     cd rcronym
+//!     echo "DATABASE_URL=postgres://rcronym@localhost/rcronym" > .env
+//!     diesel setup
+//!
+//! # Usage
+//!
+//! The command line interface can be used to add and remove acronyms with the `add` and `rm`
+//! commands. The `add` command requires a `--key` argument for the acronym, an optional `--regex`
+//! argument to customise the matching, and consumes the explanation on `stdin`.
+//!
+//! Acronyms in the database can be listed with the `list` command. The id number of each acronym
+//! can be used to remove it with `rm --id`.
+//!
+//! The most recent comments in the `/r/rust` subreddit are fetched, and any matches with acronyms
+//! in the database are printed.
+//!
+//! A blanket upper-case regex can be used to look for acronyms in use with the `candidates`
+//! command.
+
 #[macro_use]
 extern crate diesel;
 extern crate dotenv;
